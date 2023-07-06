7e9c463d56bb2bd0cec2499e9bad1fad9c6baa59
Som Nek <38861036+somnek@users.noreply.github.com>
2023-07-06 23:59:30 +0800

    test SliceEqual

--- utils/slice_equal_test.go
+++ utils/slice_equal_test.go
@@ -1,1 +1,23 @@
 package utils_test
+
+import (
+	"testing"
+
+	"github.com/somnek/go-utils/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSliceEuqal(t *testing.T) {
+	t.Parallel()
+	a := []string{"a", "b", "c"}
+	b := []string{"a", "b", "c"}
+	c := []string{"e", "f", "g"}
+	d := []string{"a", "b", "c", "d"}
+	e := []string{"b", "c", "a"}
+
+	assert.True(t, utils.SliceEqual(a, b))
+	assert.False(t, utils.SliceEqual(a, c))
+	assert.False(t, utils.SliceEqual(a, d))
+	assert.False(t, utils.SliceEqual(a, e))
+
+}
